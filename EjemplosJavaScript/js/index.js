7062dd99718ef710ccf60373f296f8e66ba1f13e
Ing. Ricardo Elizalde <146190407+ingRichart@users.noreply.github.com>
2024-10-25 08:43:59 -0600

    Ejercicios de clase

---
+++ EjemplosJavaScript/js/index.js
@@ -0,0 +1,7 @@
+// console.log("Hola Mundo desde mi archivo");
+// alert("Esto es un DM para el mundo");
+
+console.log(5+360);
+console.warn("Estamos dando tips a los alumnos de IDAI");
+console.info("ESTO ES MUY IMPORTANTE");
+console.error("NO debi de haber estudiado psicologia");
