e04130d77bf2d599bc08f749c06fd4ecf060e542
ingRichard <ricardo.elizaldeh@un.edu.mx>
2024-11-20 12:31:26 -0600

    Se suben las secciones de los productos y de los servicios

--- EjemploProyecto/js/app.js
+++ EjemploProyecto/js/app.js
@@ -16,3 +16,19 @@ enlaces.addEventListener("click", function(){
     menuResponsive.style.transformStyle = "0.5s"
     menuResponsive.classList.remove("active");
 });
+
+// SLIDER DE PRODUCTOS
+var  contenedor = document.querySelector('.slider'),
+     btnIzquierdo = document.getElementById("btn-izquierda"),
+     btnDerecho = document.getElementById("btn-derecha");
+
+     // EVENTO PARA EL BOTON DERECHO
+     btnDerecho.addEventListener("click", function () {
+          contenedor.scrollLeft += contenedor.offsetWidth;  
+     });
+
+     // EVENTO PARA EL BOTON IZQUIERDO
+     btnIzquierdo.addEventListener("click", function () {
+          contenedor.scrollLeft -= contenedor.offsetWidth;  
+     });
+
