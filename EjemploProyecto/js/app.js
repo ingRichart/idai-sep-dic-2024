b6c318e6be88c903463496e97e2686069025c191
Ing. Ricardo Elizalde <146190407+ingRichart@users.noreply.github.com>
2024-11-22 09:00:01 -0600

    Cambios parciales de contacto

--- EjemploProyecto/js/app.js
+++ EjemploProyecto/js/app.js
@@ -2,33 +2,58 @@ var btnMenuOpen = document.getElementById("btnMenuOpen"),
     btnMenuClose = document.getElementById("btnMenuClose"),
     menuResponsive = document.getElementById("menuBar");
 
-btnMenuOpen.addEventListener("click", function(){
+btnMenuOpen.addEventListener("click", function () {
     menuResponsive.classList.add("active");
 });
 
-btnMenuClose.addEventListener("click", function(){
+btnMenuClose.addEventListener("click", function () {
     menuResponsive.classList.remove("active");
 });
 
 var enlaces = document.getElementById("enlaces");
 
-enlaces.addEventListener("click", function(){
+enlaces.addEventListener("click", function () {
     menuResponsive.style.transformStyle = "0.5s"
     menuResponsive.classList.remove("active");
 });
 
 // SLIDER DE PRODUCTOS
-var  contenedor = document.querySelector('.slider'),
-     btnIzquierdo = document.getElementById("btn-izquierda"),
-     btnDerecho = document.getElementById("btn-derecha");
+var contenedor = document.querySelector('.slider'),
+    btnIzquierdo = document.getElementById("btn-izquierda"),
+    btnDerecho = document.getElementById("btn-derecha");
 
-     // EVENTO PARA EL BOTON DERECHO
-     btnDerecho.addEventListener("click", function () {
-          contenedor.scrollLeft += contenedor.offsetWidth;  
-     });
+// EVENTO PARA EL BOTON DERECHO
+btnDerecho.addEventListener("click", function () {
+    contenedor.scrollLeft += contenedor.offsetWidth;
+});
 
-     // EVENTO PARA EL BOTON IZQUIERDO
-     btnIzquierdo.addEventListener("click", function () {
-          contenedor.scrollLeft -= contenedor.offsetWidth;  
-     });
+// EVENTO PARA EL BOTON IZQUIERDO
+btnIzquierdo.addEventListener("click", function () {
+    contenedor.scrollLeft -= contenedor.offsetWidth;
+});
 
+var formulario = document.getElementById("formulario");
+
+function validar(e) {
+    var inputNombre = document.getElementById("nombre");
+    var inputEmail = document.getElementById("email");
+    var inputComments = document.getElementById("comentarios");
+
+    if (inputNombre.value == 0 
+        || inputEmail.value == 0 /* || es cond. O */
+        || inputComments.value == 0 ) {
+        e.preventDefault();
+        alert("Te faltan todos los campos");
+    }
+    else
+    {
+        e.preventDefault();
+        alert("Datos enviados");
+
+        inputNombre.value = "";
+        inputEmail.value = "";
+        inputComments.value = "";
+    }
+}
+
+formulario.addEventListener("submit", validar);
